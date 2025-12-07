b24bf6681fc1db4d6a9ca5d8a7ef87d88ef33c1b
GayathriSekar-code <gayathrisekar_88@yahoo.com>
2025-12-07 19:13:43 +0530

    Add files via upload

---
+++ day2/OddNumbers.java
@@ -0,0 +1,11 @@
+package week1.day2;
+
+public class OddNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i%2==1) {     					// condition for odd numbers
+                System.out.println(i);
+            }
+        }
+    }
+}
