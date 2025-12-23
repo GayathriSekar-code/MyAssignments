1a99960fe5f6e292570473361281c9e93d1c87a5
Gayathri Sekar <gayathrisekar_88@yahoo.com>
2025-12-23 13:49:49 +0530

    Firefox.java

--- day1/Firefox.java
+++ day1/Firefox.java
@@ -1,15 +1,25 @@
-package week1.day1;
+package week1.day1;  
 
 public class Firefox {
-		
-		public static void main(String[] args) {
-			
-			float browserVersion=100.2f;
-			String browserName="firefox";
-			String isVisible="true";
-			short releaseYear=1998;
-			char browserLogo='f';	
-			System.out.println(browserVersion+"\n"+browserName+"\n"+isVisible+"\n"+releaseYear+"\n"+browserLogo);
 
-		}
-	}
+    public static void main(String[] args) {
+
+        // Stores the browser version (decimal value)
+        float browserVersion = 100.2f;
+
+        // Stores the browser name
+        String browserName = "firefox";
+
+        // Stores whether the browser is visible (as String)
+        String isVisible = "true";
+
+        // Stores the year Firefox was released
+        short releaseYear = 1998;
+
+        // Stores the browser logo initial
+        char browserLogo = 'f';
+
+        // Print all values line by line using newline character
+        System.out.println(browserVersion + "\n" + browserName + "\n" + isVisible + "\n" + releaseYear + "\n" + browserLogo);
+    }
+}
