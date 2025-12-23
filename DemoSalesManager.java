855af4d3b4554cc867536361ec93c15c89ac6b06
Gayathri Sekar <gayathrisekar_88@yahoo.com>
2025-12-23 13:44:29 +0530

    DemoSalesManager.java

--- DemoSalesManager.java
+++ DemoSalesManager.java
@@ -4,14 +4,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoSalesManager {
-	
-	public static void main(String[] args) {
-	ChromeDriver driver = new ChromeDriver();
-	driver.get("http://leaftaps.com/opentaps/control/main");
-	driver.manage().window().maximize();
-	System.out.println(driver.getTitle());
-	driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
-	driver.findElement(By.name("PASSWORD")).sendKeys("crmsfa");
-	driver.findElement(By.className("decorativeSubmit")).click();
-}
+
+    public static void main(String[] args) {
+
+        // Create ChromeDriver object to launch Chrome browser
+        ChromeDriver driver = new ChromeDriver();
+
+        // Open the LeafTaps application URL
+        driver.get("http://leaftaps.com/opentaps/control/main");
+
+        // Maximize the browser window
+        driver.manage().window().maximize();
+
+        // Print the page title in the console
+        System.out.println(driver.getTitle());
+
+        // Locating username field by id and enter username
+        driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+
+        // Locating password field by name and enter password
+        driver.findElement(By.name("PASSWORD")).sendKeys("crmsfa");
+
+        // Locating login button by class name and click it
+        driver.findElement(By.className("decorativeSubmit")).click();
+
+        // Close the browser (optional)
+        // driver.quit();
+    }
 }
