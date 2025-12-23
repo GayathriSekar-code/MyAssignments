f189d2214dc137644b394f46078cac10264c1872
Gayathri Sekar <gayathrisekar_88@yahoo.com>
2025-12-23 13:50:46 +0530

    HelloTestLeaf.java

--- day1/HelloTestLeaf.java
+++ day1/HelloTestLeaf.java
@@ -1,7 +1,11 @@
-package week1.day1;
+package week1.day1;   // Package declaration
 
-public class HelloTestLeaf {public static void main(String[] args)	{
-	System.out.println("Welcome to TestLeaf!");
-}
+public class HelloTestLeaf {   // Class declaration
 
+    // Main method – program execution starts here
+	public static void main(String[] args) {   
+
+        // Print a welcome message to the console
+        System.out.println("Welcome to TestLeaf!");
+    }
 }
