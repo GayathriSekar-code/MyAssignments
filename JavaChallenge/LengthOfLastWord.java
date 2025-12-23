656fd89adb1663a1c50a5791106ff8c9c80e6346
Gayathri Sekar <gayathrisekar_88@yahoo.com>
2025-12-23 13:46:12 +0530

    LengthOfLastWord.java

--- JavaChallenge/LengthOfLastWord.java
+++ JavaChallenge/LengthOfLastWord.java
@@ -15,34 +15,42 @@ package JavaChallenge;
 
 public class LengthOfLastWord
 {
-
+    // Method to find the length of the last word in the string
     public static int lengthOfLastWord(String s) {
-    
-        // Start from the end and count characters until space is found
+
+        // Variable to store the length of the last word
         int length = 0;
-        for (int i = s.length() - 1; i >= 0; i--) 
-         {
+
+        // Loop starts from the last character of the string
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+            // Check if the current character is NOT a space
             if (s.charAt(i) != ' ') 
+            {           
+                length++; // If not a space, increment length counter
+
+            } else 
             {
-                length++;
-            } 
-            else 
-            {
-                break;
+                break;    // If a space is found, stop counting
             }
         }
-        return length;
+ 
+        return length; // Return the length of the last word
     }
 
     public static void main(String[] args) {
-    	String s1 = "Hello World";
+
+        // Input strings
+        String s1 = "Hello World";
         String s2 = "fly me to the moon";
         String s3 = "luffy is still joyboy";
 
-    	System.out.println("Input: \"" + s1 + "\" → Length of last word: " + lengthOfLastWord(s1));
+        // Print input string and length of its last word
+        System.out.println("Input: \"" + s1 + "\" → Length of last word: " + lengthOfLastWord(s1));
         System.out.println("Input: \"" + s2 + "\" → Length of last word: " + lengthOfLastWord(s2));
         System.out.println("Input: \"" + s3 + "\" → Length of last word: " + lengthOfLastWord(s3));
     }
 }
+
 
 
